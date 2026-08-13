ebeda8e11d3fac216d936eae867cd776fed2681e
huy <huygaming8117@gmail.com>
2026-08-13 08:06:09 +0700

    Initial commit

---
+++ config.lua
@@ -0,0 +1,25 @@
+Config = {
+    Team = "Pirates",
+
+    Configuration = {
+        HopWhenIdle = true,
+        HopNear = true,
+        FpsBoost = true,
+        BlackScreen = false,
+        FastAttackMode = "Remote"
+    },
+
+    Fruit = {
+        Sniper = true,
+        Fruit = {"Kitsune-Kitsune"},
+        EatFruitStore = false
+    },
+
+    Items = {
+        AutoFullyMelees = true,
+        Saber = true,
+        CursedDualKatana = false,
+        SoulGuitar = false,
+        RaceV2 = false
+    }
+}
